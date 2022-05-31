d5c67dd69b77e8a38c31c481cf0b2c9e64c91550
sergiorivera50 <sriveralopez50@gmail.com>
2022-05-31 16:52:31 +0100

    Minor refactor

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t pyftpdlib-docker .
-docker run -it --rm -p 21:21 -v $(pwd)/file-servers:/ftp/file-servers pyftpdlib-docker
+img=pyftpdlib-docker
+docker build -t $img .
+docker run -it --rm -p 21:21 -v $(pwd)/file-servers:/ftp/file-servers $img
